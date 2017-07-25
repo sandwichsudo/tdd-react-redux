b67b75fa1a5d8fdb34270f5e4007469b3f0f8c4b
Gilly Ames <gilly.ames@gmail.com>
2017-07-25 16:15:20 +0100

    Fetching repos when search component mounts

--- src/containers/RepoSearchPage/page/RepoSearchPage.js
+++ src/containers/RepoSearchPage/page/RepoSearchPage.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import RepoResultsList from '../components/RepoResultsList/RepoResultsList';
+import * as RepoSearchActions from '../actions/RepoSearchActions';
 
 export class RepoSearchPage extends Component {
   constructor(props) {
     super(props);
+  }
+
+  componentWillMount() {
+    this.props.actions.fetchRepos();
   }
 
   render() {
@@ -20,10 +26,15 @@ export class RepoSearchPage extends Component {
 
 RepoSearchPage.propTypes = {
   searchResults: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = state => ({
   searchResults: state.repoSearch.results,
 });
 
-export default connect(mapStateToProps)(RepoSearchPage);
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(RepoSearchActions, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(RepoSearchPage);
