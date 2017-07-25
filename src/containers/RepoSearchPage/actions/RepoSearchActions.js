d24fce357fed8e95040bab62fe4e06daeeee250d
Gilly Ames <gilly.ames@gmail.com>
2017-07-25 15:43:45 +0100

    Dispatching action when fetching items

--- src/containers/RepoSearchPage/actions/RepoSearchActions.js
+++ src/containers/RepoSearchPage/actions/RepoSearchActions.js
@@ -1,5 +1,10 @@
 import searchService from '../../../services/search/searchService';
+import { actionTypes } from '../RepoSearchConstants';
 
 export const fetchRepos = () => async (dispatch) => {
   const { items } = await searchService.repoSearch();
+  dispatch({
+    type: actionTypes.UPDATE_RESULTS,
+    items,
+  });
 };
