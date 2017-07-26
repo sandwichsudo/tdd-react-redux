731fc8ec137589793b00008301a5b3e04fa09725
Gilly Ames <gilly.ames@gmail.com>
2017-07-26 17:11:39 +0100

    Reducer case to update the error when request completes

--- src/containers/RepoSearchPage/reducer/RepoSearchReducer.js
+++ src/containers/RepoSearchPage/reducer/RepoSearchReducer.js
@@ -2,13 +2,16 @@ import { actionTypes } from '../RepoSearchConstants';
 
 const initialState = {
   results: [],
-  error: 'Oh dear'
+  error: ''
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.UPDATE_RESULTS: {
       return { ...state, results: action.items };
+    }
+    case actionTypes.REQUEST_COMPLETE: {
+      return { ...state, error: action.error };
     }
     default:
       return state;
