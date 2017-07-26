33ffa342cbb0012e3e6972d8f9cdcfcd9825297b
Gilly Ames <gilly.ames@gmail.com>
2017-07-26 17:11:14 +0100

    Test for updating error when request completes.

--- src/containers/RepoSearchPage/reducer/RepoSearchReducer.spec.js
+++ src/containers/RepoSearchPage/reducer/RepoSearchReducer.spec.js
@@ -3,6 +3,7 @@ import { actionTypes } from '../RepoSearchConstants';
 
 const initialState = {
   results: [],
+  error: '',
 };
 
 describe('RepoSearchReducer', () => {
@@ -10,6 +11,12 @@ describe('RepoSearchReducer', () => {
     it('should update the results', () => {
       const newState = RepoSearchReducer(initialState, { type: actionTypes.UPDATE_RESULTS, items: ['foo'] });
       expect(newState.results).toEqual(['foo']);
+    });
+  });
+  describe('REQUEST_COMPLETE', () => {
+    it('should update the error', () => {
+      const newState = RepoSearchReducer(initialState, { type: actionTypes.REQUEST_COMPLETE, error: 'foo' });
+      expect(newState.error).toEqual('foo');
     });
   });
 });
