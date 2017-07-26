fac8913adb562b0a226ab737c37e392b7178b85a
Gilly Ames <gilly.ames@gmail.com>
2017-07-26 17:01:13 +0100

    Updating request action to dispatch action once request complete, to pass error to state.

--- src/containers/RepoSearchPage/actions/RepoSearchActions.js
+++ src/containers/RepoSearchPage/actions/RepoSearchActions.js
@@ -2,9 +2,19 @@ import searchService from '../../../services/search/searchService';
 import { actionTypes } from '../RepoSearchConstants';
 
 export const fetchRepos = () => async (dispatch) => {
-  const { items } = await searchService.repoSearch();
+  let error = '';
+  try {
+    const { items } = await searchService.repoSearch();
+    dispatch({
+      type: actionTypes.UPDATE_RESULTS,
+      items,
+    });
+  }
+  catch ({ message }) {
+    error = message;
+  }
   dispatch({
-    type: actionTypes.UPDATE_RESULTS,
-    items,
+    type: actionTypes.REQUEST_COMPLETE,
+    error,
   });
 };
